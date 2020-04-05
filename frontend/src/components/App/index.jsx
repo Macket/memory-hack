ef425420c23e63f4959a2feb776fcbb17db0ba19
Oleg Shikov <ol.shikov@yandex.ru>
2020-04-05 14:53:13 +0300

    Change fonts

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from '../Home';
 import Compare from '../Compare';
-
+import './styles.css'
 
 export default class App extends React.Component {
    render() {
