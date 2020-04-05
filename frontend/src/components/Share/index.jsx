7cf18a63472107f289a1566f4f5328065369ddfc
Oleg Shikov <ol.shikov@yandex.ru>
2020-04-05 17:55:56 +0300

    Add ok post

--- frontend/src/components/Share/index.jsx
+++ frontend/src/components/Share/index.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     const urlParams = new URLSearchParams(window.location.search);
     const name = urlParams.get('name');
     const title = "Это ветеран " + name + ". Я загрузил фотографию ветерана на портал \"Дорога памяти\"."
-    const img = 'https://'+ location.host + urlParams.get('img');
+    const img = location.protocol + '//'+ location.host + urlParams.get('img');
     const vkElement = useRef()
 
     const okLink = 'https://connect.ok.ru/offer?url=https://' + location.host + '&title='+title+'&imageUrl='+img;
@@ -40,6 +40,17 @@ export default (props) => {
                 <div className="ok">
                     <div className="ok-share-button">
                         <a href={okLink} target="_blank">Расскажите о ветеране в Однокласниках</a>
+                    </div>
+                    <div className="ok-post">
+                        <div className="ok-header">
+                            <span className="ok-header-name">Ваше имя</span>
+                            <span className="ok-header-date">05 апр 2020</span>
+                        </div>
+                        <div className="ok-content">
+                            <img className="ok-hero-photo" src={img}/>
+                            <p>{title}</p>
+                        </div>
+                        <img src="/static/img/ok-buttons.png" className="ok-footer" />
                     </div>
                 </div>
             </div>
