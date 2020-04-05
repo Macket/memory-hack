5fc2097eb461720e30c7ea3dd5854e6f6d38f214
Makeev Ivan <makeev@phystech.edu>
2020-04-05 15:42:14 +0300

    __main__ in app.py

--- app.py
+++ app.py
@@ -82,3 +82,8 @@ def get_hero_info():
 @app.route('/share/')
 def get_share():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
